002920502ed192fce889d7fcd3a1cbf96b1cb03f
Cesar <cesaraugustododo@gmail.com>
2025-07-26 16:08:13 -0300

    clientes update

--- repairpc/settings.py
+++ repairpc/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clientes'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRES = (os.path.join(BASE_DIR, 'templates/static'))
+STATICFILES_DIRES = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

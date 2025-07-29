a14938440ea3da5298c3a386fe0f256f91301a56
Cesar <cesaraugustododo@gmail.com>
2025-07-29 19:55:59 -0300

    update de cadastro e db

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    cpf = models.CharField(max_length=11)
+
+def __str__(self) -> str:
+    return self.nome
+
+class Maquina(models.Model):
+    maquina = models.CharField(max_length=500)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    limpeza = models.IntegerField(default=0)
+    concertos = models.IntegerField(default=0)
+    
+    def __str__(self) -> str:
+        return self.maquina
--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente, Maquina
+import re
 
 def clientes(request):
     if request.method == "GET":
@@ -11,6 +13,25 @@ def clientes(request):
         cpf = request.POST.get('cpf')
         maquina = request.POST.get('maquina')
 
-        print(maquina)
+        cliente = Cliente.objects.filter(cpf=cpf)
+
+        if cliente.exists():
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'email': email, 'maquina': maquina})
+        
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf, 'maquina': maquina})
+
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+
+        cliente.save()
+
+        maq = Maquina(maquina=maquina, cliente=cliente)
+
+        maq.save()
 
         return HttpResponse('test')

eda76f50de2ad940d5c704204874f61350607f91
Cesar <cesaraugustododo@gmail.com>
2025-07-30 20:55:32 -0300

    add e att clientes

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -1,14 +1,34 @@
 function add_maquina() {
-    container = document.getElementById('form-maquina')
-    
-    html = `
+    const container = document.getElementById('form-maquina');
+    const valorAntigo = container.getAttribute('data-maquina') || '';  // pega do atributo
+
+    const html = `
         <br>
         <div class='row'>
             <div class='col-md'>
-                <textarea placeholder='Descrição da máquina' class='form-control' name='maquina' rows='4'></textarea>
+                <textarea placeholder='Descrição da máquina' class='form-control' name='maquina' rows='4'>${valorAntigo}</textarea>
             </div>
         </div>
-    `
+    `;
 
-    container.innerHTML = html
+    container.innerHTML = html;
+    container.style.display = 'block';
+}
+
+function exibir_form(tipo){
+    const add_cliente = document.getElementById('adicionar-cliente');
+    const att_cliente = document.getElementById('att_cliente');
+
+    if(tipo == "1"){
+        att_cliente.style.display = "none";
+        add_cliente.style.display = "block";
+    } else if(tipo == "2"){
+        att_cliente.style.display = "block";
+        add_cliente.style.display = "none";
+    }
+}
+
+function dados_cliente(){
+    cliente = document.getElementById('cliente-select')
+
 }
